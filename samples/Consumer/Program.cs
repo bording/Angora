0c5c87c135fefab9330f1d5bf36bfa12b3ebfde1
Brandon Ording <bording@gmail.com>
2017-02-27 22:18:08 -0500

    Add producer and consumer samples

---
+++ samples/Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Angora;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "rabbit"
+            };
+
+            var connection = await factory.CreateConnection("Consumer");
+
+            var channel = await connection.CreateChannel();
+
+            await channel.Queue.Declare("test", false, true, false, false, null);
+
+            await channel.Basic.Qos(0, 1, false);
+
+            var consumerTag = await channel.Basic.Consume("test", "Consumer", true, false, null, HandleIncomingMessage);
+
+            Console.WriteLine("Consumer started. Press any key to quit.");
+            Console.ReadKey();
+
+            await channel.Basic.Cancel(consumerTag);
+
+            await channel.Close();
+
+            await connection.Close();
+        }
+
+        static Task HandleIncomingMessage(Basic.DeliverState messageState)
+        {
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ samples/Producer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Angora;
+
+namespace Producer
+{
+    class Program
+    {
+        const int numberOfMessages = 1_000_000;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "rabbit"
+            };
+
+            var connection = await factory.CreateConnection("Producer");
+
+            var channel = await connection.CreateChannel();
+
+            await channel.Queue.Declare("test", false, true, false, false, null);
+
+            Console.WriteLine("Producer started. Press any key to send messages.");
+            Console.ReadKey();
+
+            for (int i = 0; i < numberOfMessages; i++)
+            {
+                var properties = new MessageProperties()
+                {
+                    ContentType = "message",
+                    AppId = "123",
+                    Timestamp = DateTime.UtcNow,
+                    Headers = new Dictionary<string, object>
+                    {
+                        {"MessageId",  i}
+                    }
+                };
+
+                await channel.Basic.Publish("", "test", true, properties, System.Text.Encoding.UTF8.GetBytes("Message Payload"));
+            }
+
+            await channel.Close();
+
+            await connection.Close();
+        }
+    }
+}
